cc9ea3d93159fc125f77693a5190bf4b0805ee7a
TwentyFifthNight <128817436+TwentyFifthNight@users.noreply.github.com>
2023-05-25 23:06:16 +0200

    Update LikesInit.php

--- database/seeders/LikesInit.php
+++ database/seeders/LikesInit.php
@@ -15,16 +15,5 @@ class LikesInit extends Seeder
      */
     public function run()
     {
-        DB::insert('INSERT INTO `likes` (`id`, `user_id`, `review_id`) VALUES
-            (1, 2, 5),
-            (2, 2, 8),
-            (3, 2, 7),
-            (4, 2, 2),
-            (5, 1, 6),
-            (6, 1, 8),
-            (7, 1, 2),
-            (8, 5, 1),
-            (9, 5, 7),
-            (10, 5, 6)');
     }
 }
